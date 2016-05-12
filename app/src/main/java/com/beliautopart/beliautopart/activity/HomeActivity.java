5b266f4311d7a0953cab05d41def629ad19d36db
d3v3 <brandon>
2016-05-12 15:54:15 +0700

    initial

---
+++ app/src/main/java/com/beliautopart/beliautopart/activity/HomeActivity.java
@@ -0,0 +1,44 @@
+package com.beliautopart.beliautopart.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.beliautopart.beliautopart.R;
+import com.beliautopart.beliautopart.helper.SessionManager;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private SessionManager sesssionmanager;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        sesssionmanager = new SessionManager(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    public void onLoginClick(View v) {
+        Intent i = new Intent(this, AuthenticationActivity.class);
+        i.putExtra("page", "login");
+        startActivity(i);
+    }
+
+    public void onRegisterClick(View v) {
+        Intent i = new Intent(this, AuthenticationActivity.class);
+        i.putExtra("page", "register");
+        startActivity(i);
+    }
+
+    public void onSearchClick(View v) {
+        Intent i = new Intent(this, SearchActivity.class);
+        startActivity(i);
+    }
+
+}
